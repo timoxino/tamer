6deccc137234acfffb8b5ee7a4d50d7e33a51fb3
Tsimafei Shchytkavets <timoxino@gmail.com>
2023-09-28 18:37:04 -0400

    deliver message once

--- terraform/pubsub-topic-subscription/main.tf
+++ terraform/pubsub-topic-subscription/main.tf
@@ -5,4 +5,5 @@ resource "google_pubsub_topic" "topic_pending_cv" {
 resource "google_pubsub_subscription" "subscription_tamer" {
   name  = "tamer_subscription"
   topic = google_pubsub_topic.topic_pending_cv.name
+  enable_exactly_once_delivery = true
 }
