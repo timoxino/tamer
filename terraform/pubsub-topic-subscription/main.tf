baf1e2fbd035f511b00a8ee5495af2b79a11abd6
Tsimafei Shchytkavets <timoxino@gmail.com>
2023-10-16 16:57:41 -0400

    set ack_deadline_seconds to 600

--- terraform/pubsub-topic-subscription/main.tf
+++ terraform/pubsub-topic-subscription/main.tf
@@ -6,4 +6,5 @@ resource "google_pubsub_subscription" "subscription_tamer" {
   name  = "tamer_subscription"
   topic = google_pubsub_topic.topic_pending_cv.name
   enable_exactly_once_delivery = true
+  ack_deadline_seconds = 600
 }
