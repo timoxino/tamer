0e4efeba706e57d7267adc4650c925e049b71f22
Tsimafei Shchytkavets <timoxino@gmail.com>
2024-06-30 20:35:09 -0400

    switch to spring-ai

--- src/test/java/com/timoxino/interview/tamer/TamerApplicationTests.java
+++ src/test/java/com/timoxino/interview/tamer/TamerApplicationTests.java
@@ -2,10 +2,8 @@ package com.timoxino.interview.tamer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@TestPropertySource(properties = { "openai.token = testToken" })
 class TamerApplicationTests {
 
 	@Test
